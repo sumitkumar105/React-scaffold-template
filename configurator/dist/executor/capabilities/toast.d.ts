d583bc12c4ee7cde68642cc564f4b08006b32d1d
sumitkumar105 <sumitdeshpande105@gmail.com>
2026-02-06 23:39:37 +0530

    package commands updates

--- configurator/dist/executor/capabilities/toast.d.ts
+++
@@ -1,6 +0,0 @@
-import type { Plan } from '../../planner/index.js';
-/**
- * Execute Sonner toast installation
- */
-export declare function executeToast(projectPath: string, plan: Plan): Promise<void>;
-//# sourceMappingURL=toast.d.ts.map
